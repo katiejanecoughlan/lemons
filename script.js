a22019bf82fb95eacbb461bb57844a30baaf763b
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-01-19 21:19:47 +0000

    Add remaining lemon images

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ document.body.style.backgroundImage = `url('${getImageForLemon(chosenLemon)}')`;
 function getImageForLemon(lemon) {
 // Map each lemon to its corresponding image
 const lemonImages = {
-    "Sorrento Lemon": "sorrento-lemon.jpg",
-    "Citron": "citron.jpg",
+    "Sorrento Lemon": "/assets/images/sorrento.jpg",
+    "Citron": "/assets/images/citron.png",
     "Tesco Finest Lemon": "/assets/images/tesco-lemon.png",
     "Lime": "https://images.pexels.com/photos/2363347/pexels-photo-2363347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 };
