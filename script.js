19dc0936534847c810a037d9c2c5cf38ba65ebb3
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-01-19 21:08:46 +0000

    Add images for lemon output

--- script.js
+++ script.js
@@ -17,8 +17,23 @@ function whichLemon() {
 
   const message = `You are a ${chosenLemon}!`;
 
-  // Display the message on the webpage
-  document.getElementById('output').innerHTML = message;
+// Display the message on the webpage
+document.getElementById('lemonMessage').innerHTML = message;
 
+// Apply different background images based on the selected lemon
+document.body.style.backgroundImage = `url('${getImageForLemon(chosenLemon)}')`;
+}
+
+function getImageForLemon(lemon) {
+// Map each lemon to its corresponding image
+const lemonImages = {
+    "Sorrento Lemon": "sorrento-lemon.jpg",
+    "Citron": "citron.jpg",
+    "Tesco Finest Lemon": "/assets/images/tesco-lemon.png",
+    "Lime": "https://images.pexels.com/photos/2363347/pexels-photo-2363347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+};
+
+// Return the image URL for the given lemon
+return lemonImages[lemon] || "default-image.jpg";
 }
 
